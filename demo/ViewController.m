5e0eef481c3861ce852ce4f4fee4eb415bfa6449
530098379 <530098379@qq.com>
2016-04-10 19:27:27 +0800

    2

--- demo/ViewController.m
+++ demo/ViewController.m
@@ -51,8 +51,8 @@
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
 }
-- (IBAction)doneSubmit:(id)sender {
-    NSString *userTable = @"M_USER";
+- (IBAction)doneSubmit:(UIButton *)sender {
+    /*//NSString *userTable = @"M_USER";
     //NSString *userId = @"ID";
     //NSString *userName = @"NAME";
 
@@ -60,13 +60,23 @@
         NSString * sql = [NSString stringWithFormat:
                           @"SELECT * FROM %@",userTable];
         FMResultSet * rs = [db executeQuery:sql];
-        while ([rs next]) {
+        while (![rs next]) {
             //int Id = [rs intForColumn:userId];
             //NSString * name = [rs stringForColumn:userName];
+            UIAlertController *controller =
+            [UIAlertController alertControllerWithTitle:@"登陆失败"
+                                                message:nil preferredStyle:UIAlertControllerStyleActionSheet];
+            
+            UIPopoverPresentationController *ppc = controller.popoverPresentationController;
+            if (ppc != nil) {
+                ppc.sourceView = sender;
+                ppc.sourceRect = sender.bounds;
+            }
+            [self presentViewController:controller animated:YES completion:nil];
         }
         [db close];
     }
-    /*HUD = [[MBProgressHUD alloc] initWithView:self.tabBarController.view];
+    HUD = [[MBProgressHUD alloc] initWithView:self.tabBarController.view];
     [tabBarViewController.view addSubview:HUD];
     
     HUD.delegate = self;
@@ -74,6 +84,24 @@
     
     [HUD showWhileExecuting:@selector(myTask) onTarget:self withObject:nil animated:YES];
     */
+    if (true) {
+        UIAlertController *controller =
+        [UIAlertController alertControllerWithTitle:@"帐号或密码错误，请重新填写。"
+                                            message:nil preferredStyle:UIAlertControllerStyleAlert];
+        UIAlertAction *yesAction = [UIAlertAction actionWithTitle:@"确定"
+                                                            style:UIAlertActionStyleDefault
+                                                          handler:nil];
+        [controller addAction:yesAction];
+        
+        UIPopoverPresentationController *ppc = controller.popoverPresentationController;
+        if (ppc != nil) {
+            ppc.sourceView = sender;
+            ppc.sourceRect = sender.bounds;
+        }
+        [self presentViewController:controller animated:YES completion:nil];
+    }else{
+        
+    }
 }
 
 - (IBAction)textFieldDoneEditing:(id)sender {
